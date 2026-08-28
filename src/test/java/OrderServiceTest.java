272a56793d6c38e44608e08b7e2a5179a44a499e
Josefin Vingeskog <151967701+xeffie@users.noreply.github.com>
2026-08-28 12:27:58 +0200

    Feature/order service (#3)
    * Add OrderService and productRepository
    
    * Add orderservice witrh Mockito test

---
+++ src/test/java/OrderServiceTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderServiceTest {
+
+    @Mock
+    ProductRepository productRepository;
+
+    @InjectMocks
+    OrderService orderService;
+
+    @Test
+    void shouldCalculateTotalForProduct() {
+        Product product = new Product(1, "Mjölk", 25.0);
+
+        when(productRepository.findById(1))
+                .thenReturn(product);
+
+        double result = orderService.calculateTotal(1, 2);
+
+        assertEquals(50.0, result);
+
+        verify(productRepository).findById(1);
+    }
+}
